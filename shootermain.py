e0560362265437214bc0b96454d7d44b69617207
John Cardullo <johncardullo2@gmail..com>
2020-01-18 12:00:53 -0800

    Compute Angle

--- shootermain.py
+++ shootermain.py
@@ -1,15 +1,23 @@
+import math
 
 def main():
     print("Welcome to the Shooter Calculator")
 
-    xvalue = input("Enter an x value: ")
-    
-    yvalue = input("Enter a y value: ")
-    print("x="+ xvalue)
-    print("y=" + yvalue)
+    x1= int( input("Enter an x1 value: "))
+    y1= int( input("Enter a y1 value: "))
 
-    zvalue = int(xvalue) + int(yvalue)
-    print("z=" + str(zvalue))
+    x2= int( input("Enter an x2 value: "))
+    y2= int( input("Enter a y2 value: "))
+
+    print("A=(" ,x1,",",y1,"), B=(" ,x2,",", y2 ,")")
+    slope = (y2 - y1)/ (x2 - x1)
+    print("slope=" , slope)
+
+    perp_slope = 1/slope * (-1)
+    print("perpendicular slope=" , perp_slope)
+
+    angle = math.atan(perp_slope)
+    print("angle=" ,angle)
 
 if __name__ == '__main__':
     main()
