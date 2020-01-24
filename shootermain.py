c81b3805012175492eaf0db42afa126b80e70f61
John Cardullo <johncardullo2@gmail..com>
2020-01-23 18:13:06 -0800

    Finished Shooter Calculator

--- shootermain.py
+++ shootermain.py
@@ -8,6 +8,11 @@ def main():
 
     x2= int( input("Enter an x2 value: "))
     y2= int( input("Enter a y2 value: "))
+    
+    h= (y1 + y2)/2
+    g= 32.2
+
+    velocity= float( input("Enter velocity:"))
 
     print("A=(" ,x1,",",y1,"), B=(" ,x2,",", y2 ,")")
     slope = (y2 - y1)/ (x2 - x1)
@@ -16,8 +21,18 @@ def main():
     perp_slope = 1/slope * (-1)
     print("perpendicular slope=" , perp_slope)
 
-    angle = math.atan(perp_slope)
-    print("angle=" ,angle)
+    angle_radians = math.atan(perp_slope)
+    angle_degree = angle_radians * 180/math.pi
+
+    hmax = h + velocity * velocity * math.sin(angle_radians) * math.sin(angle_radians) / (2 * g)
+    
+    print("angle in degrees=" ,angle_degree)
+
+    print("velocity=",velocity)
+
+    print("Initial Height=", h)
+
+    print("Max Height=", hmax)
 
 if __name__ == '__main__':
     main()
